1215ecd14c7519bd15c74202404369710a8a088c
Eric Silva <eric.fernando.gs@gmail.com>
2016-04-18 22:01:15 -0300

    include list class and html (view) include list class (negocio) rename repository class

--- src/br/gov/pe/educacao/repositorio/RepositorioSistemaHibernate.java
+++ src/br/gov/pe/educacao/repositorio/RepositorioSistemaHibernate.java
@@ -3,5 +3,5 @@ package br.gov.pe.educacao.br.gov.pe.educacao.repositorio;
 /**
  * Created by eric.silva on 18/04/2016.
  */
-public class RepositorioClienteHibernate {
+public class RepositorioSistemaHibernate {
 }
---
+++ src/br/gov/pe/educacao/view/ListarSistema.java
@@ -0,0 +1,7 @@
+package br.gov.pe.educacao.view;
+
+/**
+ * Created by eric.silva on 18/04/2016.
+ */
+public class ListarSistema {
+}
